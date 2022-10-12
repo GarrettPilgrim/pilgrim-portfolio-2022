3680e983d0c0fc657adfb222842eab3d102dbbad
Garrett Pilgrim <pilgrimgarrett@gmail.comgit config --global user.name Garrett>
2022-10-12 09:47:51 -0500

    Moved h2 style to global, added info object for reusable data

--- src/components/example-grid/example-grid.jsx
+++ src/components/example-grid/example-grid.jsx
@@ -1,33 +1,51 @@
 import React from "react";
 import './example-grid.scss';
+import { jobs } from "../../info/info";
 
 class Grid extends React.Component {
   constructor(props) {
     super(props)
   }
 
+
   render() {
     return (
-       <>
+      <>
         <div className="example-grid">
           <div className="example-grid-item">
             <img src="https://placekitten.com/500/500" />
-            <h2>Hello There</h2>
+            <div>
+              <h2>{jobs.Udeany.title}<br />
+                <span>{jobs.Udeany.company}</span>
+              </h2>
+            </div>
           </div>
           <div className="example-grid-item">
             <img />
-            <h2>Hello There</h2>
+            <div>
+              <h2>{jobs.Adjunct.title}<br />
+                <span>{jobs.Adjunct.company}</span>
+              </h2>
+            </div>
           </div>
           <div className="example-grid-item">
             <img />
-            <h2>Hello There</h2>
+            <div>
+              <h2>{jobs.Udeany.title}<br />
+                <span>company</span>
+              </h2>
+            </div>
           </div>
           <div className="example-grid-item">
             <img />
-            <h2>Hello There</h2>
+            <div>
+              <h2>{jobs.Udeany.title}<br />
+                <span>company</span>
+              </h2>
+            </div>
           </div>
         </div>
-       </>
+      </>
     );
   }
 }
---
+++ src/info/info.js
@@ -0,0 +1,94 @@
+const jobs = {
+  Udeany: {
+    title: "Marketing and Technology Partner",
+    company: "Udeany",
+    start: "February 2021",
+    end: "Present",
+    description: "At Udeany, I lead the marketing and technology efforts to grow the brand. This includes building and maintaining websites, Teachable courses, and CRM software. I increased the annual sales by 33% in 2021 and streamlined communication processes with an automated email marketing campaign to save work hours which lead to a 31% conversion rate from contact form submission to client. In April 2022, I lead the development of a niche landing page which increased sales by 50% from April to May and a further 12.5% from May to June setting a company sales record.",
+  },
+  Mayflower: {
+    title: "Founder",
+    company: "Mayflower Originals",
+    start: "January 2020",
+    end: "Present",
+    description: "",
+  },
+  Adjunct: {
+    title: "Adjunct Professor",
+    company: "The University of Memphis",
+    start: "August 2020",
+    end: "Present",
+    description: <p>Line one <br /> line two</p>,
+  },
+  Graduate: {
+    title: "Graduate Assistant",
+    company: "The University of Memphis",
+    start: "August 2018",
+    end: "May 2020",
+    description: "",
+  },
+  Director: {
+    title: "Firm Director",
+    company: "Meeman 901 Strategies",
+    start: "August 2017",
+    end: "May 2018",
+    description: "",
+  },
+  Lead: {
+    title: "Account Lead",
+    company: "Meeman 901 Strategies",
+    start: "August 2016",
+    end: "August 2017",
+    description: "",
+  },
+  Signature: {
+    title: "Digital Media Intern",
+    company: "Signature Advertising",
+    start: "June 2017",
+    end: "August 2017",
+    description: "",
+  },
+  Baptist: {
+    title: "Marketing Intern",
+    company: "Baptist Memorial Health Care",
+    start: "June 2016",
+    end: "August 2017",
+    description: "",
+  },
+  Choose901: {
+    title: "Blogger",
+    company: "City Leadership — Choose901",
+    start: "October 2014",
+    end: "August 2016",
+    description: "",
+  },
+}
+
+const dev = {
+  PilgrimWedding: {
+    site: "Pilgrimwedding.com",
+    url: "https://pilgrimagewedding.netlify.app/",
+  },
+  Caivrs: {
+    site: "ClearCaivrs.com",
+    url: "https://clearcaivrs.com/",
+  },
+  Pslf: {
+    site: "PSLFWaiver.co",
+    url: "https://pslfwaiver.co/",
+  },
+  Teachable: {
+    site: "courses.Udeany.com",
+    url: "https://courses.udeany.com/",
+  },
+}
+
+const design = {
+  
+}
+
+const motion = {
+
+}
+
+export { jobs, dev };
