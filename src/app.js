5ce55d6e803b8ffd6c79194e67e54ec0ea21fac7
Garrett Pilgrim <pilgrimgarrett@gmail.comgit config --global user.name Garrett>
2022-11-03 10:52:21 -0500

    Refactored page imports in App

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, createRef } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,11 +12,11 @@ import Header from "./components/header/header";
 
 // MAIN PAGES
 import Home from "./pages/home";
-import Dev from "./pages/dev"
-import Design from "./pages/design"
-import Motion from "./pages/motion"
-import Resume from "./pages/resume"
-import About from "./pages/about"
+import Dev from "./pages/dev";
+import Design from "./pages/design";
+import Motion from "./pages/motion";
+import Resume from "./pages/resume";
+import About from "./pages/about";
 
 // const Home = lazy(() => import("./pages/home"));
 // const Dev = lazy(() => import("./pages/dev"));
@@ -26,30 +26,103 @@ import About from "./pages/about"
 // const About = lazy(() => import("./pages/about"));
 
 // SUBPAGES
-import ClearCaivrs from "./pages/dev/clearcaivrs"
-import PilgrimWedding from "./pages/dev/pilgrimwedding"
-import PSLFWaiver from "./pages/dev/pslfwaiver"
-import Teachable from "./pages/dev/teachable"
+import ClearCaivrs from "./pages/dev/clearcaivrs";
+import PilgrimWedding from "./pages/dev/pilgrimwedding";
+import PSLFWaiver from "./pages/dev/pslfwaiver";
+import Teachable from "./pages/dev/teachable";
 
-import Ahsoka from "./pages/design/ahsokatano"
-import Chulo from "./pages/design/chulo"
-import Waititi from "./pages/design/taikawaititi"
-import WoodlandHills from "./pages/design/woodlandhills"
+import Ahsoka from "./pages/design/ahsokatano";
+import Chulo from "./pages/design/chulo";
+import Waititi from "./pages/design/taikawaititi";
+import WoodlandHills from "./pages/design/woodlandhills";
 
-import Donut from "./pages/motion/3ddonut"
-import UdeanyMotion from "./pages/motion/udeanymotion"
-// const ClearCaivrs = lazy(() => import("./pages/dev/clearcaivrs"));
-// const PilgrimWedding = lazy(() => import("./pages/dev/pilgrimwedding"));
-// const PSLFWaiver = lazy(() => import("./pages/dev/pslfwaiver"));
-// const Teachable = lazy(() => import("./pages/dev/teachable"));
+import Donut from "./pages/motion/3ddonut";
+import UdeanyMotion from "./pages/motion/udeanymotion";
 
-// const Ahsoka = lazy(() => import("./pages/design/ahsokatano"));
-// const Chulo = lazy(() => import("./pages/design/chulo"));
-// const Waititi = lazy(() => import("./pages/design/taikawaititi"));
-// const WoodlandHills = lazy(() => import("./pages/design/woodlandhills"));
-
-// const Donut = lazy(() => import("./pages/motion/3ddonut"));
-// const UdeanyMotion = lazy(() => import("./pages/motion/udeanymotion"));
+const routes = [
+  // MAIN PAGES
+  { path: "/", name: "Home", element: <Home />, nodeRef: createRef() },
+  { path: "/dev", name: "Dev", element: <Dev />, nodeRef: createRef() },
+  {
+    path: "/resume",
+    name: "Resume",
+    element: <Resume />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/design",
+    name: "Design",
+    element: <Design />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/motion",
+    name: "Motion",
+    element: <Motion />,
+    nodeRef: createRef(),
+  },
+  // SUB PAGES
+  {
+    path: "/dev/clearcaivrs",
+    name: "Clear Caivrs",
+    element: <ClearCaivrs />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/dev/pslfwaiver",
+    name: "PSLF Waiver",
+    element: <PSLFWaiver />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/dev/teachable",
+    name: "Teachable",
+    element: <Teachable />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/dev/pilgrimwedding",
+    name: "Pilgrim Wedding",
+    element: <PilgrimWedding />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/design/ahsokatano",
+    name: "Ahsoka Tano",
+    element: <Ahsoka />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/design/chulo",
+    name: "Chulo",
+    element: <Chulo />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/design/taikawaititi",
+    name: "Taika Waititi",
+    element: <Waititi />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/design/woodlandhills",
+    name: "Woodland Hills",
+    element: <WoodlandHills />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/motion/3ddonut",
+    name: "3D Donut",
+    element: <Donut />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/motion/woodlandhills",
+    name: "Udeany Motion",
+    element: <UdeanyMotion />,
+    nodeRef: createRef(),
+  },
+];
 
 const App = () => {
   // const location = useLocation()
