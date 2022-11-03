83e2af6a4b1adf2cf9c0df62d02d745e0f165916
Garrett Pilgrim <pilgrimgarrett@gmail.com>
2022-11-03 14:36:20 -0500

    Reworked index for page transitions

--- src/app.js
+++ src/app.js
@@ -1,170 +1,170 @@
-import React, { Suspense, createRef } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-  useOutlet,
-} from "react-router-dom";
-import ScrollToTop from "./components/scroll-to-top/scroll-to-top";
-import Footer from "./components/footer/footer";
-import Header from "./components/header/header";
+// import React, { Suspense, createRef } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   useLocation,
+//   useOutlet,
+// } from "react-router-dom";
+// import ScrollToTop from "./components/scroll-to-top/scroll-to-top";
+// import Footer from "./components/footer/footer";
+// import Header from "./components/header/header";
 
-// MAIN PAGES
-import Home from "./pages/home";
-import Dev from "./pages/dev";
-import Design from "./pages/design";
-import Motion from "./pages/motion";
-import Resume from "./pages/resume";
-import About from "./pages/about";
+// // MAIN PAGES
+// import Home from "./pages/home";
+// import Dev from "./pages/dev";
+// import Design from "./pages/design";
+// import Motion from "./pages/motion";
+// import Resume from "./pages/resume";
+// import About from "./pages/about";
 
-// const Home = lazy(() => import("./pages/home"));
-// const Dev = lazy(() => import("./pages/dev"));
-// const Motion = lazy(() => import("./pages/motion"));
-// const Design = lazy(() => import("./pages/design"));
-// const Resume = lazy(() => import("./pages/resume"));
-// const About = lazy(() => import("./pages/about"));
+// // const Home = lazy(() => import("./pages/home"));
+// // const Dev = lazy(() => import("./pages/dev"));
+// // const Motion = lazy(() => import("./pages/motion"));
+// // const Design = lazy(() => import("./pages/design"));
+// // const Resume = lazy(() => import("./pages/resume"));
+// // const About = lazy(() => import("./pages/about"));
 
-// SUBPAGES
-import ClearCaivrs from "./pages/dev/clearcaivrs";
-import PilgrimWedding from "./pages/dev/pilgrimwedding";
-import PSLFWaiver from "./pages/dev/pslfwaiver";
-import Teachable from "./pages/dev/teachable";
+// // SUBPAGES
+// import ClearCaivrs from "./pages/dev/clearcaivrs";
+// import PilgrimWedding from "./pages/dev/pilgrimwedding";
+// import PSLFWaiver from "./pages/dev/pslfwaiver";
+// import Teachable from "./pages/dev/teachable";
 
-import Ahsoka from "./pages/design/ahsokatano";
-import Chulo from "./pages/design/chulo";
-import Waititi from "./pages/design/taikawaititi";
-import WoodlandHills from "./pages/design/woodlandhills";
+// import Ahsoka from "./pages/design/ahsokatano";
+// import Chulo from "./pages/design/chulo";
+// import Waititi from "./pages/design/taikawaititi";
+// import WoodlandHills from "./pages/design/woodlandhills";
 
-import Donut from "./pages/motion/3ddonut";
-import UdeanyMotion from "./pages/motion/udeanymotion";
+// import Donut from "./pages/motion/3ddonut";
+// import UdeanyMotion from "./pages/motion/udeanymotion";
 
-const routes = [
-  // MAIN PAGES
-  { path: "/", name: "Home", element: <Home />, nodeRef: createRef() },
-  { path: "/dev", name: "Dev", element: <Dev />, nodeRef: createRef() },
-  {
-    path: "/resume",
-    name: "Resume",
-    element: <Resume />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/design",
-    name: "Design",
-    element: <Design />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/motion",
-    name: "Motion",
-    element: <Motion />,
-    nodeRef: createRef(),
-  },
-  // SUB PAGES
-  {
-    path: "/dev/clearcaivrs",
-    name: "Clear Caivrs",
-    element: <ClearCaivrs />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/dev/pslfwaiver",
-    name: "PSLF Waiver",
-    element: <PSLFWaiver />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/dev/teachable",
-    name: "Teachable",
-    element: <Teachable />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/dev/pilgrimwedding",
-    name: "Pilgrim Wedding",
-    element: <PilgrimWedding />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/design/ahsokatano",
-    name: "Ahsoka Tano",
-    element: <Ahsoka />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/design/chulo",
-    name: "Chulo",
-    element: <Chulo />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/design/taikawaititi",
-    name: "Taika Waititi",
-    element: <Waititi />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/design/woodlandhills",
-    name: "Woodland Hills",
-    element: <WoodlandHills />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/motion/3ddonut",
-    name: "3D Donut",
-    element: <Donut />,
-    nodeRef: createRef(),
-  },
-  {
-    path: "/motion/woodlandhills",
-    name: "Udeany Motion",
-    element: <UdeanyMotion />,
-    nodeRef: createRef(),
-  },
-];
+// const routes = [
+//   // MAIN PAGES
+//   { path: "/", name: "Home", element: <Home />, nodeRef: createRef() },
+//   { path: "/dev", name: "Dev", element: <Dev />, nodeRef: createRef() },
+//   {
+//     path: "/resume",
+//     name: "Resume",
+//     element: <Resume />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/design",
+//     name: "Design",
+//     element: <Design />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/motion",
+//     name: "Motion",
+//     element: <Motion />,
+//     nodeRef: createRef(),
+//   },
+//   // SUB PAGES
+//   {
+//     path: "/dev/clearcaivrs",
+//     name: "Clear Caivrs",
+//     element: <ClearCaivrs />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/dev/pslfwaiver",
+//     name: "PSLF Waiver",
+//     element: <PSLFWaiver />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/dev/teachable",
+//     name: "Teachable",
+//     element: <Teachable />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/dev/pilgrimwedding",
+//     name: "Pilgrim Wedding",
+//     element: <PilgrimWedding />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/design/ahsokatano",
+//     name: "Ahsoka Tano",
+//     element: <Ahsoka />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/design/chulo",
+//     name: "Chulo",
+//     element: <Chulo />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/design/taikawaititi",
+//     name: "Taika Waititi",
+//     element: <Waititi />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/design/woodlandhills",
+//     name: "Woodland Hills",
+//     element: <WoodlandHills />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/motion/3ddonut",
+//     name: "3D Donut",
+//     element: <Donut />,
+//     nodeRef: createRef(),
+//   },
+//   {
+//     path: "/motion/woodlandhills",
+//     name: "Udeany Motion",
+//     element: <UdeanyMotion />,
+//     nodeRef: createRef(),
+//   },
+// ];
 
-const App = () => {
-  // const location = useLocation()
-  // const currentOutlet = useOutlet()
-  return (
-    <Router>
-      <Header />
-      <Suspense>
-        <ScrollToTop>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/dev" element={<Dev />}></Route>
-            <Route path="/design" element={<Design />}></Route>
-            <Route path="/motion" element={<Motion />}></Route>
-            <Route path="/resume" element={<Resume />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            {/* sub pages */}
-            <Route path="/dev/clearcaivrs" element={<ClearCaivrs />}></Route>
-            <Route
-              path="/dev/pilgrimwedding"
-              element={<PilgrimWedding />}
-            ></Route>
-            <Route path="/dev/pslfwaiver" element={<PSLFWaiver />}></Route>
-            <Route path="/dev/teachable" element={<Teachable />}></Route>
-            <Route path="/design/ahsokatano" element={<Ahsoka />}></Route>
-            <Route path="/design/chulo" element={<Chulo />}></Route>
-            <Route path="/design/taikawaititi" element={<Waititi />}></Route>
-            <Route
-              path="/design/woodlandhills"
-              element={<WoodlandHills />}
-            ></Route>
-            <Route path="/motion/3ddonut" element={<Donut />}></Route>
-            <Route
-              path="/motion/udeanymotion"
-              element={<UdeanyMotion />}
-            ></Route>
-          </Routes>
-        </ScrollToTop>
-      </Suspense>
-      <Footer />
-    </Router>
-  );
-};
+// const App = () => {
+//   // const location = useLocation()
+//   // const currentOutlet = useOutlet()
+//   return (
+    // <Router>
+    //   <Header />
+    //   <Suspense>
+    //     <ScrollToTop>
+    //       <Routes>
+    //         <Route path="/" element={<Home />}></Route>
+    //         <Route path="/dev" element={<Dev />}></Route>
+    //         <Route path="/design" element={<Design />}></Route>
+    //         <Route path="/motion" element={<Motion />}></Route>
+    //         <Route path="/resume" element={<Resume />}></Route>
+    //         <Route path="/about" element={<About />}></Route>
+    //         {/* sub pages */}
+    //         <Route path="/dev/clearcaivrs" element={<ClearCaivrs />}></Route>
+    //         <Route
+    //           path="/dev/pilgrimwedding"
+    //           element={<PilgrimWedding />}
+    //         ></Route>
+    //         <Route path="/dev/pslfwaiver" element={<PSLFWaiver />}></Route>
+    //         <Route path="/dev/teachable" element={<Teachable />}></Route>
+    //         <Route path="/design/ahsokatano" element={<Ahsoka />}></Route>
+    //         <Route path="/design/chulo" element={<Chulo />}></Route>
+    //         <Route path="/design/taikawaititi" element={<Waititi />}></Route>
+    //         <Route
+    //           path="/design/woodlandhills"
+    //           element={<WoodlandHills />}
+    //         ></Route>
+    //         <Route path="/motion/3ddonut" element={<Donut />}></Route>
+    //         <Route
+    //           path="/motion/udeanymotion"
+    //           element={<UdeanyMotion />}
+    //         ></Route>
+    //       </Routes>
+    //     </ScrollToTop>
+    //   </Suspense>
+    //   <Footer />
+    // </Router>
+//   );
+// };
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,195 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./app";
+import { createRef } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+  Router,
+  useLocation,
+  useOutlet,
+} from "react-router-dom";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 import "./styles.scss";
-import "./base.scss";
+import "./transitions.scss";
 
-class Index extends React.Component {
-  render() {
-    return (
-      <>
-        <App />
-      </>
-    );
-  }
+// MAIN PAGES
+import Home from "./pages/home";
+import Dev from "./pages/dev";
+import Design from "./pages/design";
+import Motion from "./pages/motion";
+import Resume from "./pages/resume";
+import About from "./pages/about";
+import ErrorPage from "./pages/404";
+
+// SUB PAGES
+import ClearCaivrs from "./pages/dev/clearcaivrs";
+import PilgrimWedding from "./pages/dev/pilgrimwedding";
+import PSLFWaiver from "./pages/dev/pslfwaiver";
+import Teachable from "./pages/dev/teachable";
+
+import Ahsoka from "./pages/design/ahsokatano";
+import Chulo from "./pages/design/chulo";
+import Waititi from "./pages/design/taikawaititi";
+import WoodlandHills from "./pages/design/woodlandhills";
+
+import Donut from "./pages/motion/3ddonut";
+import UdeanyMotion from "./pages/motion/udeanymotion";
+
+const routes = [
+  // MAIN PAGES
+  { path: "/", name: "Home", element: <Home />, nodeRef: createRef() },
+  { path: "/dev", name: "Dev", element: <Dev />, nodeRef: createRef() },
+  {
+    path: "/resume",
+    name: "Resume",
+    element: <Resume />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/design",
+    name: "Design",
+    element: <Design />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/motion",
+    name: "Motion",
+    element: <Motion />,
+    nodeRef: createRef(),
+  },
+  // SUB PAGES
+  {
+    path: "/dev/clearcaivrs",
+    name: "Clear Caivrs",
+    element: <ClearCaivrs />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/dev/pslfwaiver",
+    name: "PSLF Waiver",
+    element: <PSLFWaiver />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/dev/teachable",
+    name: "Teachable",
+    element: <Teachable />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/dev/pilgrimwedding",
+    name: "Pilgrim Wedding",
+    element: <PilgrimWedding />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/design/ahsokatano",
+    name: "Ahsoka Tano",
+    element: <Ahsoka />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/design/chulo",
+    name: "Chulo",
+    element: <Chulo />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/design/taikawaititi",
+    name: "Taika Waititi",
+    element: <Waititi />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/design/woodlandhills",
+    name: "Woodland Hills",
+    element: <WoodlandHills />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/motion/3ddonut",
+    name: "3D Donut",
+    element: <Donut />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/motion/woodlandhills",
+    name: "Udeany Motion",
+    element: <UdeanyMotion />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/404",
+    name: "Page Does Not Exist",
+    element: <ErrorPage />,
+    nodeRef: createRef(),
+  },
+];
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+    children: routes.map((route) => ({
+      index: route.path === "/",
+      path: route.path === "/" ? undefined : route.path,
+      element: route.element,
+    })),
+  },
+]);
+
+function Index() {
+  const location = useLocation();
+  const currentOutlet = useOutlet();
+  const { nodeRef } =
+    routes.find((route) => route.path === location.pathname) ?? {};
+  return (
+    <>
+      <Header />
+      <SwitchTransition>
+        <CSSTransition
+          key={location.pathname}
+          nodeRef={nodeRef}
+          timeout={300}
+          classNames="page"
+          unmountOnExit
+        >
+          {(state) => (
+            <div ref={nodeRef} className="page">
+              {currentOutlet}
+            </div>
+          )}
+        </CSSTransition>
+      </SwitchTransition>
+      <Footer />
+    </>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Index />);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<RouterProvider router={router} fallbackElement={<ErrorPage />} />);
+
+// INDEX PRIOR TO TRANSITIONS
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./app";
+// import "./styles.scss";
+// import "./base.scss";
+
+// class Index extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <App />
+//       </>
+//     );
+//   }
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Index />);
