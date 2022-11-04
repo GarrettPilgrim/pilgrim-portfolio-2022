659433de52172927bc7715e9a4598024c88dbb70
Garrett Pilgrim <pilgrimgarrett@gmail.comgit config --global user.name Garrett>
2022-11-04 10:31:18 -0500

    Refactored classes in ahsokatano and clearcaivrs, added text to ahsokatano

--- src/pages/design/ahsokatano.jsx
+++ src/pages/design/ahsokatano.jsx
@@ -2,16 +2,29 @@ import React from "react";
 import ExamplePage from "../../components/example-page/example-page";
 import { design } from "../../info/info";
 
-class Ahsoka extends React.Component {
-  render() {
-    return (
-      <>
-        <ExamplePage
-          name={design.Ahsoka.name}
-        />
-      </>
-    );
-  }
-}
+const Ahsoka = () => {
+  return (
+    <>
+      <ExamplePage name={design.Ahsoka.name}>
+        <h2>The Idea</h2>
+        <p>
+          Star Wars is my favorite fictional universe and has been since
+          childhood. I grew fond of the Clone Wars TV series, and as soon as
+          Ahsoka made her live-action debut in The Mandalorian, I was giddy.
+        </p>
+        <p>
+          The beautiful film work on her first episode inspired me to recreate a
+          moment using Adobe Fresco. I used it as a challenge to practice my
+          illustration skills and digitally paint Ahsoka.
+        </p>
+        <h3>The Tools</h3>
+        <ul>
+          <li>Illustrator</li>
+          <li>Fresco</li>
+        </ul>
+      </ExamplePage>
+    </>
+  );
+};
 
 export default Ahsoka;
