29a0d9e14fae1ab8b78f6378624684c7ae7a491b
Garrett Pilgrim <pilgrimgarrett@gmail.comgit config --global user.name Garrett>
2022-12-26 11:23:29 -0600

    Added willywonka dev page

--- src/pages/dev/willywonka.jsx
+++ src/pages/dev/willywonka.jsx
@@ -11,18 +11,34 @@ const WillyWonka = () => {
         img={dev.WillyWonka.cover}
         alt={dev.WillyWonka.alt}
       >
-        <h2>The Problem</h2>
-
-        <h2>The Strategy</h2>
-
-        <h2>The Solution</h2>
-
-        <h2>The Results</h2>
-
+        <h2>The Challenge</h2>
+        <p>
+          The UI for the site was built using pure HTML and CSS live in class to
+          demonstrate coding techniques and principles to students.
+        </p>
+        <p>
+          These live coding session show the process of buidling a site and
+          modern layout techniques such as grid and flex.
+        </p>
+        <h2>Refactoring</h2>
+        <p>
+          After the live coding sessions, I refactored the site into the Next.js
+          framework.
+        </p>
+        <p>
+          I created product pages with dynamic rendering based on markdown data
+          and refined design system with DRY code and component-based design.
+        </p>
         <h3>The Tools</h3>
         <ul>
-          <li></li>
-          <li></li>
+          <li>APIs</li>
+          <li>Component-based design</li>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>Javascript</li>
+          <li>Next.js</li>
+          <li>React</li>
+          <li>UI/UX</li>
         </ul>
       </ExamplePage>
     </>
